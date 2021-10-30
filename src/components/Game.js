c11813b5834b81a10a7d0e82c5eb3f146a0486f3
zeechapman <zeechapman@gmail.com>
2021-10-30 10:09:32 -0700

    Card shuffle working

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import Cards from '../assets/cards.json';
+import Libs from '../assets/libs.json';
 
 class Game extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cards: Cards,
+            cardsShuffled: ''
+        }
+    }
+
+    shuffle() {
+        return this.state.cards.sort(() => Math.random() - 0.5);
+    }
+
+    componentDidMount() {
+        this.setState({cardsShuffled: this.shuffle()});
+    }
+
     render() {
         return (
             <div className="Game">
-                <h1>Game Start</h1>
             </div>
         );
     }
